a0a31b87909a36d6b49af9f972b34e60206dc099
Omar Zekri <102541219+Omar50x@users.noreply.github.com>
2022-10-14 18:21:22 +0100

    Add Login And Logout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import MasterLayout from "./layouts/admin/MasterLayout";
 import Home from './components/frontend/Home';
 import Login from "./components/frontend/auth/Login";
 import Regiter from "./components/frontend/auth/Register";
+import AdminPrivateRoute from './AdminPrivateRoute'
+import Page403 from "./components/errors/Page403";
+import Page404 from "./components/errors/Page404";
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://127.0.0.1:8000/"
@@ -23,6 +26,8 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/403" component={Page403} />
+          <Route exact path="/404" component={Page404} />
           {/* <Route path="/login" component={Login} />
           <Route path="/register" component={Regiter} /> */}
           <Route path="/login">
@@ -31,7 +36,8 @@ function App() {
           <Route path="/register">
             {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Regiter />}
           </Route>
-          <Route path="/admin" name="Admin" render={(props) => <MasterLayout {...props} />} />
+          {/* <Route path="/admin" name="Admin" render={(props) => <MasterLayout {...props} />} /> */}
+          <AdminPrivateRoute path="/admin" name="Admin" />
         </Switch>
       </Router>
     </div>
